958d2642e11fe386d164d4f9f8e28c5ce202c11c
Centbhi <centbhi@example.com>
2026-05-09 09:27:16 -0400

    feat:__init__

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, headers=headers, json=input_json)
+    response_dict = json.loads(response.text)
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+
+    dominant = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = dominant
+
+    print(emotions)
+    
